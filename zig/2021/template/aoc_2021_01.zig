c9ba2d84ccd1a597385a57fea300b2e47c070090
AB036 <ab036@meteorcall.com>
2021-11-30 22:42:36 +0100

    prepare 2021 zig

---
+++ zig/2021/template/aoc_2021_01.zig
@@ -0,0 +1,35 @@
+const std = @import("std");
+
+const print = std.debug.print;
+
+pub fn read_lines(path: []const u8, allocator: *std.mem.Allocator) ![][]const u8 {
+    const cwd = std.fs.cwd();
+    const file = try cwd.openFile(path, .{ .read = true });
+    defer file.close();
+
+    const file_buffer = try file.readToEndAlloc(allocator, 9999999);
+    const trimmed = std.mem.trim(u8, file_buffer, "\n");
+    var iterator = std.mem.tokenize(u8, trimmed, "\n");
+
+    var list = std.ArrayList([]const u8).init(allocator);
+    while (iterator.next()) |token| {
+        try list.append(token);
+    }
+
+    return list.toOwnedSlice();
+}
+
+
+pub fn main() !void {
+    var arena = std.heap.ArenaAllocator.init(std.heap.page_allocator);
+    defer arena.deinit();
+    const allocator = &arena.allocator;
+
+    //~ var lines = try read_lines("input.txt", allocator);
+    var lines = try read_lines("test.txt", allocator);
+
+    
+
+}
+
+
