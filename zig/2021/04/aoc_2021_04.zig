72dd831f5920751aaca8541319d1abec6d451596
AB036 <ab036@meteorcall.com>
2021-12-04 16:00:46 +0100

    refactor 2021 04

--- zig/2021/04/aoc_2021_04.zig
+++ zig/2021/04/aoc_2021_04.zig
@@ -12,40 +12,31 @@ pub fn read_input(path: []const u8, allocator: *std.mem.Allocator) !std.mem.Spli
     return std.mem.split(u8, trimmed, "\n\n"); // double newline here
 }
 
-const XY = struct { x: u8, y: u8 };
-
 const Bingo = struct {
-    grid: std.AutoHashMap(u8, XY),
-    marks: std.ArrayList(bool),
+    inv_grid: std.AutoHashMap(u8, u8),
+    marks: [25]bool = [_]bool{false} ** 25,
+    complete: bool = false,
+    ignore: bool = false,
     
-    pub fn mark(self: Bingo, number: u8) void {
-        var xy = self.grid.get(number);  // may be null if number isn't in the grid
-        if (xy != null) {self.marks.items[5*xy.?.y + xy.?.x] = true;}
+    pub fn mark(self: *Bingo, number: u8) void {
+        const index = self.inv_grid.get(number) orelse return;
+        self.marks[index] = true;
+        
+        const x: u8 = index % 5; var row_complete = true;
+        const y: u8 = index / 5; var col_complete = true;
+        var d: u8 = 0;
+        while (d < 5) : (d += 1) {
+            row_complete = row_complete and self.marks[5*d + x];
+            col_complete = col_complete and self.marks[5*y + d];
+        }
+        if (row_complete or col_complete) {self.complete = true;}
     }
     
-    pub fn is_complete(self: Bingo) bool {
-        var y: u8 = 0;  // check columns
-        while (y < 5) : (y += 1) {
-            var x: u8 = 0; var complete = true;
-            while (x < 5) : (x += 1) {complete = complete and self.marks.items[5*y+x];}
-            if (complete) {return true;}
-        }
-        
-        var x: u8 = 0;  // check lines
-        while (x < 5) : (x += 1) {
-            y = 0; var complete = true;
-            while (y < 5) : (y += 1) {complete = complete and self.marks.items[5*y+x];}
-            if (complete) {return true;}
-        }
-        
-        return false;
-    }
-    
-    pub fn sum_unmarked(self: Bingo) u32 {
+    pub fn sum_unmarked(self: *Bingo) u32 {
         var sum: u32 = 0;
-        var iterator = self.grid.iterator();
+        var iterator = self.inv_grid.iterator();
         while (iterator.next()) |kv| {
-            if (! self.marks.items[5*kv.value_ptr.y + kv.value_ptr.x]) {sum += kv.key_ptr.*;}
+            if (!self.marks[kv.value_ptr.*]) {sum += kv.key_ptr.*;}
         }
         return sum;
     }
@@ -60,45 +51,40 @@ pub fn main() !void {
     var iterator = try read_input("input.txt", allocator);
     //~ var iterator = try read_input("test.txt", allocator);
     
-    const first_line = iterator.next().?;
-    var ite1 = std.mem.tokenize(u8, first_line, ",");
-    var numbers = std.ArrayList(u8).init(allocator);
-    while (ite1.next()) |token| {
-        try numbers.append(try std.fmt.parseInt(u8, token, 10));
-    }
+    const first_line = iterator.next().?;  // will parse later
 
-    var bingos = std.ArrayList(Bingo).init(allocator);
-    var bingos_done = std.ArrayList(bool).init(allocator);
+    var bingos = std.ArrayList(*Bingo).init(allocator);
     while (iterator.next()) |grid_text| {
-        var marks = std.ArrayList(bool).init(allocator);
-        var map = std.AutoHashMap(u8, XY).init(allocator);
-        
+        var inv_grid = std.AutoHashMap(u8, u8).init(allocator);
+        var i: u8 = 0;
         var ite2 = std.mem.tokenize(u8, grid_text, "\n");
-        var y: u8 = 0;
-        while (ite2.next()) |line| : (y += 1){
+        while (ite2.next()) |line| {
             var ite3 = std.mem.tokenize(u8, line, " ");
-            var x: u8 = 0;
-            while (ite3.next()) |number| : (x += 1){
-                try map.put(try std.fmt.parseInt(u8, number, 10), XY {.x = x, .y = y});
-                try marks.append(false);
+            while (ite3.next()) |number_str| {
+                try inv_grid.put(try std.fmt.parseInt(u8, number_str, 10), i);
+                i += 1;
             }
         }
         
-        try bingos.append(Bingo {.grid = map, .marks = marks});
-        try bingos_done.append(false);
+        const bingo_ptr = try allocator.create(Bingo);
+        bingo_ptr.* = Bingo {.inv_grid = inv_grid} ;
+        try bingos.append(bingo_ptr);
     }
     
     var n_bingos_left = bingos.items.len;
     
-    for (numbers.items) |number| {
-        for (bingos.items) |bingo, k| {
+    var ite1 = std.mem.tokenize(u8, first_line, ",");
+    while (ite1.next()) |token| {
+        const number = try std.fmt.parseInt(u8, token, 10);
+        
+        for (bingos.items) |bingo| {
             bingo.mark(number);
-            if (bingo.is_complete() and !bingos_done.items[k]) {
+            if (bingo.complete and !bingo.ignore) {
                 if (n_bingos_left == bingos.items.len) {
                     print("Part 1: {}\n", .{bingo.sum_unmarked() * number});
                 }
                 n_bingos_left -= 1;
-                bingos_done.items[k] = true;
+                bingo.ignore = true;
                 if (n_bingos_left == 0) {
                     print("Part 2: {}\n", .{bingo.sum_unmarked() * number});
                 }
